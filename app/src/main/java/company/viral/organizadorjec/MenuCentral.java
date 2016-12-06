6529d0ac6b7a1ecd21febbe94a62d6ee7a37d8ed
eRny <ernyhidalgo@gmail.com>
2016-12-06 19:58:30 -0400

    TODO EN ORDEN!
    ahora si TOOODO EN ORDEN!!

--- app/src/main/java/company/viral/organizadorjec/MenuCentral.java
+++ app/src/main/java/company/viral/organizadorjec/MenuCentral.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +15,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import company.viral.organizadorjec.Fragment.AjustesF;
 import company.viral.organizadorjec.Fragment.CalendarioF;
+import company.viral.organizadorjec.Fragment.ConfiguracionActividadF;
+import company.viral.organizadorjec.Fragment.ConfiguracionMateriaF;
+import company.viral.organizadorjec.Fragment.ConfiguracionPeriodoF;
+import company.viral.organizadorjec.Fragment.ConfiguracionProfesorF;
 import company.viral.organizadorjec.Fragment.InicioF;
 import company.viral.organizadorjec.Fragment.PerfilF;
 import company.viral.organizadorjec.Fragment.ProfesoresF;
@@ -23,12 +33,16 @@ import company.viral.organizadorjec.Fragment.ProfesoresF;
 public class MenuCentral extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private PopupWindow popupadicion;
+    private DrawerLayout posicionpopup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_central);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        posicionpopup = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         //colocamos el fragment con que inicia el menu
 
@@ -40,14 +54,82 @@ public class MenuCentral extends AppCompatActivity
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
+            //metodo de escucha para el popup
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                //implementamos el popup
+                LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                final View vistaadicion = inflater.inflate(R.layout.activity_pop_adicion,null);
+
+                popupadicion = new PopupWindow(
+                        vistaadicion, RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+
+                //luego de clicear y abrir el popup le decimos...
+                //si das al profe ve a profe
+                LinearLayout btnprofe = (LinearLayout) vistaadicion.findViewById(R.id.btnagregarprofesor);
+                btnprofe.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.contenedor,new ConfiguracionProfesorF()).commit();
+                        popupadicion.dismiss();
+
+                    }
+                });
+                //si le das actividad ve actividad
+                LinearLayout btnactividad = (LinearLayout) vistaadicion.findViewById(R.id.btnagregaractividad);
+                btnactividad.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.contenedor,new ConfiguracionActividadF()).commit();
+                        popupadicion.dismiss();
+
+                    }
+                });
+                //si le das a materias ve a materias
+                LinearLayout btnmaterias = (LinearLayout) vistaadicion.findViewById(R.id.btnagregarmateria);
+                btnmaterias.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.contenedor,new ConfiguracionMateriaF()).commit();
+                        popupadicion.dismiss();
+
+                    }
+                });
+                //si le das a periodo ve a periodo
+                LinearLayout btnperiodo = (LinearLayout) vistaadicion.findViewById(R.id.btnagregarperiodo);
+                btnperiodo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.contenedor,new ConfiguracionPeriodoF()).commit();
+                        popupadicion.dismiss();
+
+                    }
+                });
+                //luego le decimos que cierre el popup con el boton
+
+                Button cerrarboton = (Button) vistaadicion.findViewById(R.id.btnpopupcerrar);
+                cerrarboton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupadicion.dismiss();
+                    }
+                });
+                //hubicamos donde queremos el popup
+                popupadicion.showAtLocation(posicionpopup, Gravity.CENTER,0,0 );
             }
         });
-
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
